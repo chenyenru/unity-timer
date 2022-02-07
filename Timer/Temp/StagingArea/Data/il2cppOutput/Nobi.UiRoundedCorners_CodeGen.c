a55a00a9584a1f3f8c4fb47b17cd31244ca68856
ChenYenRu <rebecca946487@gmail.com>
2022-02-07 10:08:24 +0800

    update

---
+++ Timer/Temp/StagingArea/Data/il2cppOutput/Nobi.UiRoundedCorners_CodeGen.c
@@ -0,0 +1,87 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void ImageWithIndependentRoundedCorners::OnRectTransformDimensionsChange()
+extern void ImageWithIndependentRoundedCorners_OnRectTransformDimensionsChange_mE64E2A2BE43B09BC59CA144B826E07E42FD0001E (void);
+// 0x00000002 System.Void ImageWithIndependentRoundedCorners::OnValidate()
+extern void ImageWithIndependentRoundedCorners_OnValidate_m30ECDF4AA10896B38B38E0C140A1F41216E2BDA6 (void);
+// 0x00000003 System.Void ImageWithIndependentRoundedCorners::RecalculateProps(UnityEngine.Vector2)
+extern void ImageWithIndependentRoundedCorners_RecalculateProps_mF66C74F17BB040238B6DF870C439D84770E1605B (void);
+// 0x00000004 System.Void ImageWithIndependentRoundedCorners::Refresh()
+extern void ImageWithIndependentRoundedCorners_Refresh_m6FABC4381480E4B8ED09BF08A9C6185952A5A4A2 (void);
+// 0x00000005 System.Void ImageWithIndependentRoundedCorners::.ctor()
+extern void ImageWithIndependentRoundedCorners__ctor_m438D2FB361071E98936373406A01A50654FAC4E7 (void);
+// 0x00000006 System.Void ImageWithIndependentRoundedCorners::.cctor()
+extern void ImageWithIndependentRoundedCorners__cctor_m7CD014EDFEE93E47785D843014F6FF58AE0AEA78 (void);
+// 0x00000007 System.Void ImageWithRoundedCorners::OnRectTransformDimensionsChange()
+extern void ImageWithRoundedCorners_OnRectTransformDimensionsChange_m3A8C65589D785495CDB7EB4D8B6883887968588F (void);
+// 0x00000008 System.Void ImageWithRoundedCorners::OnValidate()
+extern void ImageWithRoundedCorners_OnValidate_m7C9097A6EE0169EC5FF66935ECD79E41ABF7056D (void);
+// 0x00000009 System.Void ImageWithRoundedCorners::Refresh()
+extern void ImageWithRoundedCorners_Refresh_m80E15E48CBB2BAEC0715FB8E1DA77D908CFD58CC (void);
+// 0x0000000A System.Void ImageWithRoundedCorners::.ctor()
+extern void ImageWithRoundedCorners__ctor_m3806BF6B3018B31F5759562DFE99FEF9762F68CC (void);
+// 0x0000000B System.Void ImageWithRoundedCorners::.cctor()
+extern void ImageWithRoundedCorners__cctor_mE5107CA4250D6B8D017990A1840D9776A33BC56B (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	ImageWithIndependentRoundedCorners_OnRectTransformDimensionsChange_mE64E2A2BE43B09BC59CA144B826E07E42FD0001E,
+	ImageWithIndependentRoundedCorners_OnValidate_m30ECDF4AA10896B38B38E0C140A1F41216E2BDA6,
+	ImageWithIndependentRoundedCorners_RecalculateProps_mF66C74F17BB040238B6DF870C439D84770E1605B,
+	ImageWithIndependentRoundedCorners_Refresh_m6FABC4381480E4B8ED09BF08A9C6185952A5A4A2,
+	ImageWithIndependentRoundedCorners__ctor_m438D2FB361071E98936373406A01A50654FAC4E7,
+	ImageWithIndependentRoundedCorners__cctor_m7CD014EDFEE93E47785D843014F6FF58AE0AEA78,
+	ImageWithRoundedCorners_OnRectTransformDimensionsChange_m3A8C65589D785495CDB7EB4D8B6883887968588F,
+	ImageWithRoundedCorners_OnValidate_m7C9097A6EE0169EC5FF66935ECD79E41ABF7056D,
+	ImageWithRoundedCorners_Refresh_m80E15E48CBB2BAEC0715FB8E1DA77D908CFD58CC,
+	ImageWithRoundedCorners__ctor_m3806BF6B3018B31F5759562DFE99FEF9762F68CC,
+	ImageWithRoundedCorners__cctor_mE5107CA4250D6B8D017990A1840D9776A33BC56B,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	1089,
+	1089,
+	959,
+	1089,
+	1089,
+	1813,
+	1089,
+	1089,
+	1089,
+	1089,
+	1813,
+};
+extern const CustomAttributesCacheGenerator g_Nobi_UiRoundedCorners_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_Nobi_UiRoundedCorners_CodeGenModule;
+const Il2CppCodeGenModule g_Nobi_UiRoundedCorners_CodeGenModule = 
+{
+	"Nobi.UiRoundedCorners.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_Nobi_UiRoundedCorners_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
